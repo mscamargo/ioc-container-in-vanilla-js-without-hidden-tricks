ed0954433c1a0711592efdb6ca5ef8d380142d82
mscamargo <mscamargo@pm.me>
2022-11-09 21:47:27 -0300

    pushing v2
    yeah this is a lazy commit

--- main.mjs
+++ main.mjs
@@ -1,68 +1,133 @@
 import { randomUUID } from 'node:crypto'
+export const Inject = Symbol('IoC::ProviderDependencies')
 
 export class Container {
-    providers = new Map()
-    instances = new Map()
+  #registry = new Map()
+  #singletons = new Map()
 
-    register (providerClass, dependencies = []) {
-        const isClass = typeof providerClass === 'function' && providerClass.toString().startsWith('class')
-        if (!isClass) {
-            throw new Error('The provider must be a class reference')
-        }
-        this.providers.set(providerClass.name, { providerClass, dependencies })
+  register (ClassOrToken, valueOrFactory) {
+    if (!this.#isValid(ClassOrToken)) {
+      throw new Error('Invalid token type')
     }
+    if (this.#isClass(ClassOrToken)) {
+      this.#registry.set(ClassOrToken.name, ClassOrToken)
+    } else {
+      if (!valueOrFactory) {
+        throw new Error('For a normal token a value or a factory provider is required')
+      }
+      this.#registry.set(ClassOrToken, valueOrFactory)
+    }
+    return this
+  }
 
-    resolve (providerClass) {
-        if (!this.providers.has(providerClass.name)) {
-            throw new Error('Provider not found')
-        }
-        const provider = this.providers.get(providerClass.name)
-        const dependencies = provider.dependencies.map((dependency) => this.resolve(dependency))
-        const instance = new provider.providerClass(...dependencies)
-        this.instances.set(providerClass.name, instance)
-        return instance
+  resolve (ClassOrToken) {
+    const token = ClassOrToken.name ?? ClassOrToken
+    if (!this.isRegistered(ClassOrToken)) {
+      throw new Error(`The provider ${token.toString()} is not registered`)
     }
+    if (!this.#singletons.has(token)) {
+      const Provider = this.#registry.get(token)
+      let instance = Provider
+      if (this.#isClass(Provider)) {
+        const dependencies = Provider[Inject]?.map(dependency => this.resolve(dependency)) ?? []
+        instance = new Provider(...dependencies)
+      } else if (typeof Provider === 'function') {
+        instance = Provider()
+      }
+      this.#singletons.set(token, instance)
+    }
+    return this.#singletons.get(token)
+  }
+
+  isRegistered (tokenOrClass) {
+    return this.#registry.has(
+      this.#isClass(tokenOrClass) ? tokenOrClass.name : tokenOrClass
+    )
+  }
+
+  #isValid (token) {
+    return this.#isSymbol(token) ||
+        this.#isString(token) ||
+        this.#isClass(token)
+  }
+
+  #isSymbol (token) {
+    return typeof token === 'symbol'
+  }
+
+  #isString (token) {
+    return typeof token === 'string'
+  }
+
+  #isClass (token) {
+    return typeof token === 'function' && token.toString().startsWith('class')
+  }
 }
 
-class AddTodoController {
-    constructor (addTodoService) {
-        this.addTodoService = addTodoService
-    }
+/*******************************************************************************
+ * EXAMPLES
+ */
 
-    handle (name) {
-        return this.addTodoService.add(name)
+const TODO_COLLECTION = Symbol('TodoRegistry')
+const ID_GENERATOR = 'ID_GENERATOR'
+
+class TodoRepository {
+  static [Inject] = [
+    TODO_COLLECTION,
+    ID_GENERATOR
+  ]
+
+  constructor (todoCollection, idGenerator) {
+    this.todoCollection = todoCollection
+    this.idGenerator = idGenerator
+  }
+
+  add (name) {
+    const todo = {
+      id: this.idGenerator.generate(),
+      name
     }
+    this.todoCollection.add(todo)
+    return todo
+  }
 }
 
 class TodoService {
-    constructor (addTodoRepository) {
-        this.addTodoRepository = addTodoRepository
-    }
+  static [Inject] = [
+    TodoRepository
+  ]
 
-    add (name) {
-        return this.addTodoRepository.add(name)
-    }
+  constructor (addTodoRepository) {
+    this.addTodoRepository = addTodoRepository
+  }
+
+  add (name) {
+    return this.addTodoRepository.add(name)
+  }
 }
 
-class TodoRepository {
-    todos = []
+class AddTodoController {
+  static [Inject] = [
+    TodoService
+  ]
 
-    add (name) {
-        const todo = {
-            id: randomUUID(),
-            name
-        }
-        this.todos.push(todo)
-        return todo
-    }
+  constructor (addTodoService) {
+    this.addTodoService = addTodoService
+  }
+
+  handle (name) {
+    return this.addTodoService.add(name)
+  }
 }
 
 const container = new Container()
-container.register(AddTodoController, [TodoService])
-container.register(TodoService, [TodoRepository])
-container.register(TodoRepository)
+  .register(AddTodoController)
+  .register(TodoService)
+  .register(TodoRepository)
+  .register(TODO_COLLECTION, new Set())
+  .register(ID_GENERATOR, () => ({ generate: () => randomUUID() }))
 
 const controller = container.resolve(AddTodoController)
 
-console.log({ controller })
 console.log(controller.handle('estudar solid'))
+console.log(controller)
--- test.mjs
+++ test.mjs
@@ -1,99 +1,164 @@
 import { test } from 'node:test'
 import assert from 'node:assert/strict'
-import { Container } from './main.mjs'
-
-class AnyProvider {}
-
-class AnyProviderWithDeps {
-    constructor (dep1) {
-        this.dep1 = dep1
-    }
-}
-
-class Dep1 {}
-
-class Dep1WithDeps {
-    constructor (dep2) {
-        this.dep2 = dep2
-    }
-}
-
-class Dep2 {
-
-}
+import { Container, Inject } from './main.mjs'
 
 test('Container.register()', async (t) => {
-    await t.test('should only allow class references as providers', () => {
-        assert.throws(
-            () => new Container().register('wrong class reference'),
-            { message: 'The provider must be a class reference' }
-        )
+  const invalidTypeValues = [true, 1, {}, () => {}, null, undefined, NaN]
+  for (const value of invalidTypeValues) {
+    await t.test(`should NOT accept the type ${typeof value} as token`, () => {
+      assert.throws(
+        () => new Container().register(value),
+        { message: 'Invalid token type' }
+      )
     })
+  }
 
-    // first version
-    // await t.test('should add the provider to the providers collection', () => {
-    //     const container = new Container()
-    //     container.register(AnyProvider)
-    //     assert.ok(container.providers.get(AnyProvider.name))
-    // })
-    //
-    // await t.test('should register the provider as a class reference', () => {
-    //     const container = new Container()
-    //     container.register(AnyProvider)
-    //     const registeredProvider = container.providers.get(AnyProvider.name)
-    //     assert.ok(typeof registeredProvider === 'function')
-    //     assert.ok(registeredProvider.toString().startsWith('class'))
-    // })
+  await t.test('should accept the type string as token', () => {
+    assert.ok(new Container().register('a string', 'any value provider') instanceof Container)
+  })
 
-    await t.test('should register the provider with its dependencies', () => {
-        const container = new Container()
-        container.register(AnyProviderWithDeps, [Dep1])
-        const { providerClass, dependencies } = container.providers.get(AnyProviderWithDeps.name)
-        assert.ok(typeof providerClass === 'function')
-        assert.ok(providerClass.toString().startsWith('class'))
-        assert.deepEqual(dependencies, [Dep1])
-    })
+  await t.test('should accept the type symbol as token', () => {
+    assert.ok(new Container().register(Symbol('any'), 'any value provider') instanceof Container)
+  })
+
+  await t.test('should accept a class reference as token', () => {
+    assert.ok(new Container().register(class {}) instanceof Container)
+  })
+
+  await t.test('should register a class provider correctly', () => {
+    const container = new Container()
+
+    class Provider {}
+
+    container.register(Provider)
+    assert.ok(container.isRegistered(Provider))
+  })
+
+  await t.test('should require a value or factory provider when a normal token is provided', () => {
+    assert.throws(
+      () => new Container().register('normal'),
+      { message: 'For a normal token a value or a factory provider is required' }
+    )
+  })
+
+  await t.test('should register a factory provider correctly', () => {
+    const container = new Container()
+    container.register('provider', () => {})
+    assert.ok(container.isRegistered('provider'))
+  })
 })
 
 test('Container.resolve()', async (t) => {
-    await t.test('should validate if the provider exists in the providers collection', () => {
-        assert.throws(() => new Container().resolve(AnyProvider),
-            { message: 'Provider not found' }
-        )
+  await t.test('should instantiate a class', () => {
+    class Provider {}
 
-    })
-    await t.test('should register a provider instance', () => {
-        const container = new Container()
-        container.register(AnyProvider)
-        container.resolve(AnyProvider)
-        const instance = container.instances.get(AnyProvider.name)
-        assert.ok(instance instanceof AnyProvider)
-    })
+    const instance = new Container()
+      .register(Provider)
+      .resolve(Provider)
+    assert.ok(instance instanceof Provider)
+  })
 
-    await t.test('should return the provider instance', () => {
-        const container = new Container()
-        container.register(AnyProvider)
-        const instance = container.resolve(AnyProvider)
-        assert.ok(instance instanceof AnyProvider)
-    })
+  await t.test('should resolve the dependencies', () => {
+    class Provider1 {}
 
-    await t.test('should resolve the provider dependencies', () => {
-        const container = new Container()
-        container.register(AnyProviderWithDeps, [Dep1])
-        container.register(Dep1)
-        const instance = container.resolve(AnyProviderWithDeps)
-        assert.ok(instance instanceof AnyProviderWithDeps)
-        assert.ok(instance.dep1 instanceof Dep1)
-    })
+    class Provider2 {
+      static [Inject] = [Provider1]
 
-    await t.test('should resolve dependencies from provider dependencies', () => {
-        const container = new Container()
-        container.register(AnyProviderWithDeps, [Dep1WithDeps])
-        container.register(Dep1WithDeps, [Dep2])
-        container.register(Dep2)
-        const instance = container.resolve(AnyProviderWithDeps)
-        assert.ok(instance instanceof AnyProviderWithDeps)
-        assert.ok(instance.dep1 instanceof Dep1WithDeps)
-        assert.ok(instance.dep1.dep2 instanceof Dep2)
-    })
+      constructor (provider1) {
+        this.provider1 = provider1
+      }
+    }
+
+    const container = new Container()
+      .register(Provider1)
+      .register(Provider2)
+    const instance = container.resolve(Provider2)
+    assert.ok(instance instanceof Provider2)
+    assert.ok(instance.provider1 instanceof Provider1)
+  })
+
+  await t.test('should resolve the dependencies recursively', () => {
+    class Provider1 {}
+
+    class Provider2 {
+      static [Inject] = [Provider1]
+
+      constructor (provider1) {
+        this.provider1 = provider1
+      }
+    }
+
+    class Provider3 {
+      static [Inject] = [Provider2]
+
+      constructor (provider2) {
+        this.provider2 = provider2
+      }
+    }
+
+    const container = new Container()
+      .register(Provider1)
+      .register(Provider2)
+      .register(Provider3)
+    const instance = container.resolve(Provider3)
+    assert.ok(instance instanceof Provider3)
+    assert.ok(instance.provider2 instanceof Provider2)
+    assert.ok(instance.provider2.provider1 instanceof Provider1)
+  })
+
+  await t.test('should use the singleton pattern to resolve dependencies', () => {
+    class Provider {
+      static constructionCount = 0
+      constructor () {
+        Provider.constructionCount++
+      }
+    }
+    const container = new Container()
+      .register(Provider)
+    container.resolve(Provider)
+    container.resolve(Provider)
+    assert.equal(Provider.constructionCount, 1)
+  })
+
+  await t.test('should resolve a value provider', () => {
+    class ClassProvider {
+      static [Inject] = [
+        'valueProvider'
+      ]
+
+      constructor (vp) {
+        this.vp = vp
+      }
+    }
+    const container = new Container()
+    container.register(ClassProvider)
+    container.register('valueProvider', {})
+    const instance = container.resolve(ClassProvider)
+    assert.deepEqual(instance.vp, {})
+  })
+
+  await t.test('should resolve a factory provider', () => {
+    class ClassProvider {
+      static [Inject] = [
+        'factoryProvider'
+      ]
+
+      constructor (fp) {
+        this.fp = fp
+      }
+    }
+    const container = new Container()
+    container.register(ClassProvider)
+    container.register('factoryProvider', () => ({}))
+    const instance = container.resolve(ClassProvider)
+    assert.deepEqual(instance.fp, {})
+  })
+
+  await t.test('should throw an error when the provider is not registered', () => {
+    class Provider {}
+    assert.throws(
+      () => new Container().resolve(Provider),
+      { message: `The provider ${Provider.name} is not registered` }
+    )
+  })
 })
